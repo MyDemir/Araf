dcb56083346933f366d5d0627d220fb1048e10a6
MyD <m.demir-21@outlook.com>
2026-04-29 19:39:51 +0300

    Prevent public index from overriding processed Vite output

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public-assets',
   build: {
     rollupOptions: {
       input: resolve(__dirname, 'public/index.html'),
